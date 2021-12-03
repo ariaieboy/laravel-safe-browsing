6b56171876d34d42ba99fabe0c06ca4c8f505de0
AriaieBOY <smr.seddighy@gmail.com>
2021-12-04 02:05:58 +0330

    wip

--- config/laravel-safe-browsing.php
+++ config/laravel-safe-browsing.php
@@ -1,5 +1,21 @@
 <?php
 // config for Ariaieboy/LaravelSafeBrowsing
 return [
-    'key' => env('LARAVEL_SAFE_BROWSING_KEY', null)
+    'google'=>[
+        'api_key'=>env('SAFEBROWSING_GOOGLE_API_KEY',null),
+        'timeout'=>30,
+        'threatTypes' => [
+            'THREAT_TYPE_UNSPECIFIED',
+            'MALWARE',
+            'SOCIAL_ENGINEERING',
+            'UNWANTED_SOFTWARE',
+            'POTENTIALLY_HARMFUL_APPLICATION',
+        ],
+
+        'threatPlatforms' => [
+            'ANY_PLATFORM'
+        ],
+        'clientId' => 'ariaieboy-safebrowsing',
+        'clientVersion' => '0.1.0',
+    ]
 ];
--- src/LaravelSafeBrowsing.php
+++ src/LaravelSafeBrowsing.php
@@ -5,9 +5,17 @@ namespace Ariaieboy\LaravelSafeBrowsing;
 class LaravelSafeBrowsing
 {
     protected ?string $apiKey = null;
+    protected string $clientId;
+    protected string $clientVersion;
+    protected array $threatTypes;
+    protected array $platformTypes;
 
-    public function __construct()
+    public function __construct(?string $apiKey = null)
     {
-        $this->apiKey = config('laravel-safe-browsing.api_key');
+        $this->apiKey = $apiKey ?? config('laravel-safe-browsing.google.api_key');
+        $this->clientId = config('laravel-safe-browsing.google.clientId');
+        $this->clientVersion = config('laravel-safe-browsing.google.clientVersion');
+        $this->threatTypes = config('laravel-safe-browsing.google.threatTypes');
+        $this->platformTypes = config('laravel-safe-browsing.google.threatPlatforms');
     }
 }
