801429a8e26c74c0c699d3eb98183def7094bd30
ariaieboy <ariaieboy@users.noreply.github.com>
2021-12-03 22:21:25 +0000

    Fix styling

--- tests/LaravelSafeBrowserTest.php
+++ tests/LaravelSafeBrowserTest.php
@@ -1,6 +1,7 @@
 <?php
+
 use Ariaieboy\LaravelSafeBrowsing\Facades\LaravelSafeBrowsing;
 
-it('can determine unsafe url',function(){
+it('can determine unsafe url', function () {
     $this->assertFalse(LaravelSafeBrowsing::isSafeUrl('http://malware.testing.google.test/testing/malware/'));
 });
