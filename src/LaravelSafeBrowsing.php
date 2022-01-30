7641f7bbf4bca50c25b1f2aaf11c732cc0eb8708
ariaieboy <smr.seddighy@gmail.com>
2022-01-30 15:42:18 +0330

    add isSafeUrl function with tests

--- src/LaravelSafeBrowsing.php
+++ src/LaravelSafeBrowsing.php
@@ -2,6 +2,8 @@
 
 namespace Ariaieboy\LaravelSafeBrowsing;
 
+use Illuminate\Support\Facades\Http;
+
 class LaravelSafeBrowsing
 {
     protected ?string $apiKey = null;
@@ -10,6 +12,9 @@ class LaravelSafeBrowsing
     protected array $threatTypes;
     protected array $platformTypes;
 
+    /**
+     * @throws \Exception
+     */
     public function __construct(?string $apiKey = null)
     {
         $this->apiKey = $apiKey ?? config('laravel-safe-browsing.google.api_key');
@@ -17,9 +22,54 @@ class LaravelSafeBrowsing
         $this->clientVersion = config('laravel-safe-browsing.google.clientVersion');
         $this->threatTypes = config('laravel-safe-browsing.google.threatTypes');
         $this->platformTypes = config('laravel-safe-browsing.google.threatPlatforms');
+        if (is_null($this->apiKey) or empty($this->apiKey)) {
+            throw new \Exception('API Key is required');
+        }
     }
 
-    protected function getApiResult(string $url)
+    public function isSafeUrl(string $url, bool $returnType = false): bool|string
     {
+        $result = $this->getApiResult($url);
+        if (is_array($result) and isset($result['matches'])) {
+            if ($returnType) {
+                foreach ($result['matches'] as $match) {
+                    if ($match['threat']['url'] === $url) {
+                        return $match['threatType'];
+                    }
+                }
+            }
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    protected
+    function getApiResult(string $url): array|string
+    {
+        $lookupUrl = sprintf("https://safebrowsing.googleapis.com/v4/threatMatches:find?key=%s", $this->apiKey);
+        $lookupData = [
+            'client' => [
+                'clientId' => $this->clientId,
+                'clientVersion' => $this->clientVersion,
+            ],
+            'threatInfo' => [
+                'threatTypes' => $this->threatTypes,
+                'platformTypes' => $this->platformTypes,
+                'threatEntryTypes' => ['URL'],
+                'threatEntries' => [
+                    [
+                        'url' => $url,
+                    ],
+                ],
+            ],
+        ];
+        $response = Http::post($lookupUrl, $lookupData);
+        if ($response->failed()) {
+            throw new \Exception($response->json('error.message'));
+        }
+        return $response->json();
     }
 }
--- tests/LaravelSafeBrowserTest.php
+++ tests/LaravelSafeBrowserTest.php
@@ -2,6 +2,18 @@
 
 use Ariaieboy\LaravelSafeBrowsing\Facades\LaravelSafeBrowsing;
 
+it('throw exception on missing api_key', function () {
+    config(['laravel-safe-browsing.google.api_key' => '']);
+    LaravelSafeBrowsing::isSafeUrl('http://malware.testing.google.test/testing/malware/');
+})->throws(Exception::class, 'API Key is required');
+it('will throw exception if api_key is not valid', function () {
+    config(['laravel-safe-browsing.google.api_key' => 'fake_api_key']);
+    LaravelSafeBrowsing::isSafeUrl('http://malware.testing.google.test/testing/malware/');
+})->throws(Exception::class);
 it('can determine unsafe url', function () {
     $this->assertFalse(LaravelSafeBrowsing::isSafeUrl('http://malware.testing.google.test/testing/malware/'));
+});
+it('can return threatTypes', function () {
+    $result = LaravelSafeBrowsing::isSafeUrl('http://malware.testing.google.test/testing/malware/', true);
+    $this->assertContains($result, ['MALWARE', 'SOCIAL_ENGINEERING', 'UNWANTED_SOFTWARE']);
 });
